56f66f621b451b28ae58313482d82b752ea35139
Pavel Egorov <xoposhiy@mail.ru>
2013-10-31 12:45:47 +0600

    remove trash

--- nc2013/Server/Arena/FinishedGamesWithEnemy.cs
+++ nc2013/Server/Arena/FinishedGamesWithEnemy.cs
@@ -8,18 +8,8 @@ namespace Server.Arena
 		[JsonProperty] public int Draws;
 		[JsonProperty] public string Enemy;
 		[JsonProperty] public int EnemyVersion;
-
 		[JsonProperty] public FinishedGameInfo[] LastGames;
 		[JsonProperty] public int EnemyScore;
 		[JsonProperty] public int Score;
-
-		public static FinishedGamesWithEnemy CreateDummy(int n = 0)
-		{
-			return new[]
-			{
-				new FinishedGamesWithEnemy {Enemy = "spaceorc", EnemyVersion = 3, Score = 100, EnemyScore = 20, Draws = 80, LastGames = new[] {new FinishedGameInfo()}},
-				new FinishedGamesWithEnemy {Enemy = "imp", EnemyVersion = 1, Score = 100, EnemyScore = 20, Draws = 80, LastGames = new[] {new FinishedGameInfo()}},
-			}[n];
-		}
 	}
 }
