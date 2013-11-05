350863d561ed8802979581b885ea79775881e1cb
andrew <Andrew.Kostousov@gmail.com>
2013-11-05 15:42:09 +0600

    fix rules

--- nc2013/Core/Engine/Parameters.cs
+++ nc2013/Core/Engine/Parameters.cs
@@ -3,8 +3,8 @@
 	public static class Parameters
 	{
 		public const int CoreSize = 8000;
-		public const int MaxWarriorLength = 100;
-		public const int MinWarriorsDistance = 100;
+		public const int MaxWarriorLength = 256;
+		public const int MinWarriorsDistance = 512;
 		public const int MaxQueueSize = 1000;
 		public const int MaxStepsPerWarrior = 80000;
 	}
