00cedcfd9fd85f5ce9075a0b5ba919a15a28e125
andrew <Andrew.Kostousov@gmail.com>
2013-11-05 20:04:51 +0600

    read battlesPerPair param from args

--- nc2013/Server/Program.cs
+++ nc2013/Server/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using Core;
 using Core.Arena;
 using Core.Game;
 using Core.Parser;
@@ -20,7 +19,7 @@ namespace Server
 	public static class Program
 	{
 		private const string defaultPrefix = "http://*/corewar/";
-
+		private const int defaultBattlesPerPair = 5;
 		private static readonly ILog log = LogManager.GetLogger(typeof(Program));
 
 		public static void Main([NotNull] string[] args)
@@ -32,9 +31,12 @@ namespace Server
 
 		private static void RunServer([NotNull] string[] args)
 		{
+			var staticContentPath = GetStaticContentDir();
 			var prefix = GetPrefix(args);
-			var godModeSecret = Guid.NewGuid();
-			Log.For<GameHttpServer>().Warn(string.Format("GodModeSecret: {0}", godModeSecret));
+			var battlesPerPair = GetBattlesPerPair(args);
+			var productionMode = ProductionMode(args);
+			var godAccessOnly = GodAccessOnly(args);
+			var godModeSecret = GenerateGodModeSecret();
 			var warriorProgramParser = new WarriorParser();
 			var playersRepo = new PlayersRepo(new DirectoryInfo("../players"), warriorProgramParser);
 			var gamesRepo = new GamesRepo(new DirectoryInfo("../games"));
@@ -42,9 +44,8 @@ namespace Server
 			var gameServer = new GameServer();
 			var debuggerManager = new DebuggerManager(gameServer);
 			var battleRunner = new BattleRunner();
-			var tournamentRunner = new TournamentRunner(playersRepo, gamesRepo, battleRunner, 5);
-			var godAccessOnly = GodAccessOnly(args);
-			var httpServer = new GameHttpServer(prefix, playersRepo, gamesRepo, sessionManager, debuggerManager, tournamentRunner, GetStaticContentDir(), godModeSecret, godAccessOnly);
+			var tournamentRunner = new TournamentRunner(playersRepo, gamesRepo, battleRunner, battlesPerPair);
+			var httpServer = new GameHttpServer(prefix, playersRepo, gamesRepo, sessionManager, debuggerManager, tournamentRunner, staticContentPath, godModeSecret, godAccessOnly);
 			Runtime.SetConsoleCtrlHandler(() =>
 			{
 				log.InfoFormat("Stopping...");
@@ -53,28 +54,49 @@ namespace Server
 			});
 			tournamentRunner.Start();
 			httpServer.Run();
-			log.InfoFormat("Listening {0}", prefix);
-			if (!ProductionMode(args))
+			log.InfoFormat("Listening on: {0}", prefix);
+			if (!productionMode)
 				Process.Start(httpServer.DefaultUrl);
 			httpServer.WaitForTermination();
 			tournamentRunner.WaitForTermination();
 			log.InfoFormat("Stopped");
 		}
 
+		private static Guid GenerateGodModeSecret()
+		{
+			var godModeSecret = Guid.NewGuid();
+			log.Warn(string.Format("GodModeSecret: {0}", godModeSecret));
+			return godModeSecret;
+		}
+
+		private static int GetBattlesPerPair([NotNull] IEnumerable<string> args)
+		{
+			var battlesPerPairArg = args.FirstOrDefault(x => x.StartsWith("-battlesPerPair:", StringComparison.OrdinalIgnoreCase));
+			var battlesPerPair = battlesPerPairArg == null ? defaultBattlesPerPair : int.Parse(battlesPerPairArg.Split(':')[1]);
+			log.InfoFormat("BattlesPerPair: {0}", battlesPerPair);
+			return battlesPerPair;
+		}
+
 		private static bool ProductionMode([NotNull] IEnumerable<string> args)
 		{
-			return args.Any(x => string.Equals(x, "-production", StringComparison.OrdinalIgnoreCase));
+			var productionMode = args.Any(x => string.Equals(x, "-production", StringComparison.OrdinalIgnoreCase));
+			log.InfoFormat("ProductionMode: {0}", productionMode);
+			return productionMode;
 		}
 
 		private static bool GodAccessOnly([NotNull] IEnumerable<string> args)
 		{
-			return args.Any(x => string.Equals(x, "-godAccessOnly", StringComparison.OrdinalIgnoreCase));
+			var godAccessOnly = args.Any(x => string.Equals(x, "-godAccessOnly", StringComparison.OrdinalIgnoreCase));
+			log.InfoFormat("GodAccessOnly: {0}", godAccessOnly);
+			return godAccessOnly;
 		}
 
 		private static string GetPrefix([NotNull] IEnumerable<string> args)
 		{
 			var prefix = args.FirstOrDefault();
-			return string.IsNullOrEmpty(prefix) ? defaultPrefix : prefix;
+			var result = string.IsNullOrEmpty(prefix) ? defaultPrefix : prefix;
+			log.InfoFormat("HttpListenerPrefix: {0}", result);
+			return result;
 		}
 
 		private static string GetStaticContentDir()
